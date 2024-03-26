369410119d920c3027c078ceab57f1f4e6189787
Jake <jakeparra98@yahoo.com>
2024-03-26 16:26:27 -0600

    Creates and implements navbar.html

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html>
 
+<?php
+include 'navbar.html';
+?>
+
+<head>
+    <meta name="author" content="Gabriel Bertasius, Issac Curtis, Jake Parra, Daniel Peaslee">
+    <meta name="keywords" content="HTML, CSS, JavaScript,">
+    <meta name="description" content="Home Page for T3chSharkz29 LLC">
+    <title>T3chSharkz29 LLC</title>
+</head>
+
+<body>
+    <h1> T3chSharkz29 LLC</h1>
+</body>
+
+</html>
