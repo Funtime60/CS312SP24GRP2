460002cfd85597454a0dabc0d45d53e581f9382a
Funtime60 <Funtime60@users.noreply.github.com>
2024-03-29 14:59:53 -0600

    Update colorGenerator.php

--- colorGenerator.php
+++ colorGenerator.php
@@ -34,7 +34,7 @@
 			<tbody>
 				<?php
 					for($iI = 0; $iI < $iColor; $iI++) {
-						echo '<tr id=' . $iI . '><td class="table1L"></td><td class="table1R"></td></tr>';
+						echo '<tr id=' . $iI . '><td class="table1L">test</td><td class="table1R">test</td></tr>';
 					}
 				?>		
 			</tbody>
