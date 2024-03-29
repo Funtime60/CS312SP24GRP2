4425159407ababb578d322fa614930675ed7588a
Funtime60 <Funtime60@users.noreply.github.com>
2024-03-29 14:35:07 -0600

    Update colorGenerator.php

--- colorGenerator.php
+++ colorGenerator.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
+	<head>
 		<meta name="author" content="Gabriel Bertasius, Issac Curtis, Jake Parra, Daniel Peaslee">
-        <meta name="keywords" content="HTML, CSS, JavaScript,">
-            <meta name="description" content="Color Generator Page for T3chSharkz29 LLC">
-            <title>Color Generator</title>
-        </head>
-
-
-<?php
-include 'navbar.html';
-?>
-<body>
-    <h1> T3chSharkz29 LLC</h1>
-</body>
-
+		<meta name="keywords" content="HTML, CSS, JavaScript,">
+		<meta name="description" content="Color Generator Page for T3chSharkz29 LLC">
+		<title>Color Generator</title>
+	</head>
+	<body>
+		<?php
+			include 'navbar.html';
+		?>
+		<h1> T3chSharkz29 LLC</h1>
+	</body>
 </html>
