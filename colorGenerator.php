4855aaf1bd30101636a30938fac60cb5c600fd0f
Funtime60 <Funtime60@users.noreply.github.com>
2024-03-29 14:34:06 -0600

    Update colorGenerator.php

--- colorGenerator.php
+++ colorGenerator.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="author" content="Gabriel Bertasius, Issac Curtis, Jake Parra, Daniel Peaslee">
-            <meta name="keywords" content="HTML, CSS, JavaScript,">
+		<meta name="author" content="Gabriel Bertasius, Issac Curtis, Jake Parra, Daniel Peaslee">
+        <meta name="keywords" content="HTML, CSS, JavaScript,">
             <meta name="description" content="Color Generator Page for T3chSharkz29 LLC">
             <title>Color Generator</title>
         </head>
