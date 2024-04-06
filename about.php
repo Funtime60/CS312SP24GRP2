d47ab1c4e6348cc3f0868299c819ba86f2364018
Daniel Peaslee <daniel.a.peaslee@gmail.com>
2024-04-05 18:52:37 -0600

    Cleanup

--- about.php
+++ about.php
@@ -18,17 +18,12 @@
 	    <h1 id="pageTitle"><?php echo $sTitle ?></h1>
 		<div id="peopleGrid">
 			<div class="person">
-			    <h2> Gabriel Bertasius </h2>
-			    <!-- INSERT BIO/PHOTO HERE    -->
-			</div>
-			<div class="person">
 			    <h2> Isaac Curtis </h2>
 			    <img src="./Photos/Isaac_Curtis_About_Me.jpeg" alt="Isaac Curtis standing infront of a a massive can of soup">
 			    <p>
 			        My name is Isaac Curtis. I am from Wyoming, but my family comes from around the world. My father comes from Washington and my mother comes from Wyoming. I am the Founder and President of Ramily Movie Night, which is CSU’s premiere film club on campus. I spend my free time doing very little. Feel free to reach out to me with any questions, comments, cares, concerns, statements, compliments, insults, jokes, or anything else! You know where to find me. 
 			    </p>
 			</div>
-			<div class="person">
 			<div class="person">
 			    <h2> Jake Parra</h2>
 				<img src="./Photos/JakeParraProfilePic.JPG" alt="Jake Parra posing for headshot photo">
@@ -39,7 +34,6 @@
 					software engineering. In my free time, I enjoy going to the gym, spending time with friends, and playing video games.
 				</p>
 			</div>
-			</div>
 			<div class="person">
 			    <h2> Daniel Peaslee</h2>
 			    <img src="Photos/peaslee_picture_short.jpg" alt="Daniel Peaslee standing infront a blank wall in one of several identical shirts">
@@ -47,15 +41,15 @@
 					I am a computer science major. I love everything to do with computers and aim to work with them in the future in just about any capacity. In my free time I love to play PC games and tinker with obsolete and/or uncommon PC technology.
 				</p>
 			</div>
-					<div class="person">
-			<h2>Gabriel Bertasius</h2>
-			<img src="./Photos/gabrielbertasiusabout.jpg" alt="Portrait photo of Gabriel Bertasius">
-			<p>
-				I'm Gabriel Bertasius and I'm a CS student focusing on Artificial Intelligence and Machine Learning. I enjoy being a student, but I can't wait to work with real world systems.
-				I started programming with C++ and OpenGL in my early teens and have been intrigued by AI systems in video games. I hope to gain a better understanding of how AI and ML systems work so I can apply them to agents inside virtual worlds.
-				When I'm away from my computer I like to get a good workout at the gym and in the summers I spend alot of time biking.
-			</p>
-		</div>
+			<div class="person">
+				<h2>Gabriel Bertasius</h2>
+				<img src="./Photos/gabrielbertasiusabout.jpg" alt="Portrait photo of Gabriel Bertasius">
+				<p>
+					I'm Gabriel Bertasius and I'm a CS student focusing on Artificial Intelligence and Machine Learning. I enjoy being a student, but I can't wait to work with real world systems.
+					I started programming with C++ and OpenGL in my early teens and have been intrigued by AI systems in video games. I hope to gain a better understanding of how AI and ML systems work so I can apply them to agents inside virtual worlds.
+					When I'm away from my computer I like to get a good workout at the gym and in the summers I spend alot of time biking.
+				</p>
+			</div>
 		</div>
 	</body>
 </html>
