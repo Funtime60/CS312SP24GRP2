ba94aaa3b5ab4fb876079864a6a85dc827c879e2
Daniel Peaslee <daniel.a.peaslee@gmail.com>
2024-05-02 20:03:47 -0600

    did too much oops

--- colorGenerator.js
+++ colorGenerator.js
@@ -31,23 +31,34 @@ function classColorHandler() {
 	varLines = varLines.map((x, i, a) => x.split(/(: |;)/).map((y, j) => (j == 2)?colorArr[i] ?? y:y).join(''));
 	lines.splice(2, 0, ...varLines);
 	text = lines.join("\n");
-	console.log(text);
 	$("style#colorVar").text(text);
 };
 
 function cellColorHandler() {
 	const indexArr = ["index0", "index1", "index2", "index3", "index4", "index5", "index6", "index7", "index8", "index9"];
-	console.log($(this));
 	const currColor = $("input:radio[name='colorSelectRadio']:checked")[0].value;
 	const othrColrs = indexArr.filter((x) => x != currColor);
 	$(this).removeClass(othrColrs.join(" "));
 	$(this).addClass(currColor);
+
+	colorListHandler();
+};
+
+function colorListHandler() {
+	const slctList = $("table#table1 select").toArray();
+	const otptList = slctList.map((x) => x.parentElement.nextElementSibling.firstElementChild);
+	const indxList = slctList.map((x) => x.nextElementSibling.value);
+	const cellList = indxList.map((x) => $("#table2 td.table2Cell."+x).toArray());
+	otptList.forEach((x, i, a) => $("#"+x.id).attr("value", cellList[i].map((y, j) => y.id.substring(9).split("Col").map((z, k) => (k == 0)? parseInt(z) : String.fromCharCode(65 + parseInt(z))).reverse().join("")).toSorted().join(", ")));
 };
 
 $(document).ready(function() {
 	selectLockHandler();
 	classColorHandler();
+	colorListHandler();
 	$("table#table1 select").on('change', selectLockHandler);
 	$("table#table1 select").on('change', classColorHandler);
 	$(".table2Cell").on('click', cellColorHandler);
+	// No longer used since it HAS to be called AFTER the cellColorHandler
+	//$(".table2Cell").on('click', colorListHandler);
 });
